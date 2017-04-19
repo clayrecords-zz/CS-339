39fdc856aea8d41a20c213dbafde0094c658e835
clayrecords <crecords@bsu.edu>
2017-04-18 21:49:27 -0400

    Started parsing IpRoutes.

--- CS339 V2/Router.cs
+++ CS339 V2/Router.cs
@@ -11,6 +11,7 @@ namespace CS339_V2
         public string name;
         public List<Interface> interfaces;
         public List<Vlan> vlans;
+
         public Router(string fileName, string contents)
         {
             this.name = fileName;
@@ -33,8 +34,9 @@ namespace CS339_V2
                 }
                 catch (Exception e)
                 {
-                    //   Console.WriteLine("Exception: " + e.Message);
-                    //   Console.ReadKey();
+                    //No second word
+                    //Console.WriteLine("Exception: " + e.Message);
+                    //Console.ReadKey();
                 }
             }
         }
@@ -47,21 +49,57 @@ namespace CS339_V2
                 try
                 {
                     String[] words = line.Split(' ');
-                    if (words[1] == "route")
+                    if (words[1] == "route" && words[2] != "vrf" && !words[4].Contains("Null") && !words[4].Contains("Vlan"))
                     {
-                        if (words[2] != "vrf" && !words[4].Contains("Null") && !words[4].Contains("Vlan"))
-                        {
-                            //IPRoute  = new ClasslessConnection(line);
-                            //classlessConnections.Add(cc);
-                        }
+                        Vlan vlan = findVlanThroughRoute(words[2], words[3], words[4]);
                     }
                 }
                 catch (Exception e)
                 {
+                    //no second word
                     //Console.WriteLine("Exception: " + e.Message);
                     //Console.ReadKey();
                 }
             }
+        }
+
+        private Vlan findVlanThroughRoute(string starterIP, string starterMask, string routeIP)
+        {
+            int starterIPInt = (int)IPstringToInt(starterIP);
+            int starterMaskInt = (int)IPstringToInt(starterMask);
+            string prefix = findPrefix(starterIPInt, starterMaskInt);
+            return new Vlan();
+
+        }
+        public Int64 IPstringToInt(String stringIP)
+        {
+            string[] stringIPSegments = stringIP.Split('.');
+            List<int> intIPSegments = stringIPtoIntIP(stringIPSegments);
+            Int64 intIP = Convert.ToInt64((intIPSegments[0] * Math.Pow(2, 24)) + (intIPSegments[1] * Math.Pow(2, 16)) +
+                (intIPSegments[2] * Math.Pow(2, 8)) + intIPSegments[3]);
+            return intIP;
+        }
+
+        public string findPrefix(Int64 IP, Int64 MASK)
+        {
+            Int64 prefixInt = IP & MASK;
+            List<int> temp = new List<int>();
+            temp.Add((int)((prefixInt / Math.Pow(2, 24)) % (Math.Pow(2, 8))));
+            temp.Add((int)((prefixInt / Math.Pow(2, 16)) % (Math.Pow(2, 8))));
+            temp.Add((int)((prefixInt / Math.Pow(2, 8)) % (Math.Pow(2, 8))));
+            temp.Add((int)(prefixInt % Math.Pow(2, 8)));
+            return temp[0] + "." + temp[1] + "." + temp[2] + "." + temp[3];
+        }
+
+        public List<int> stringIPtoIntIP(string[] stringSegments)
+        {
+            List<int> intSegments = new List<int>();
+            foreach (string stringSegment in stringSegments)
+            {
+                int intSegment = int.Parse(stringSegment);
+                intSegments.Add(intSegment);
+            }
+            return intSegments;
         }
 
         private void findInterfaces(string[] chunks)
@@ -93,4 +131,3 @@ namespace CS339_V2
         }
     }
 }
-
